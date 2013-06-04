7ede4c7f0c3c6d56020cc70a91edcd64c57ebb14
Philip Potter <philip.g.potter@gmail.com>
2013-06-04 10:44:29 +0100

    add manifest which does stuff

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,38 @@
-# == Class: ext4mount
+# == Define: ext4mount
 #
-# Full description of class ext4mount here.
+# Ensures a specified disk is mounted. Checks that the file system actually exists before trying to mount it and writing the fstab entry
+# Only works for ext4 disks.
 #
-# === Parameters
+# == Parameters
+# [*mountpoint*]
+#   Full path to the mount point
 #
-# Document parameters here.
+# [*disk*]
+#   Full path to the disk to mount
 #
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if it
-#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should not be used in preference to class parameters  as of
-#   Puppet 2.6.)
-#
-# === Examples
-#
-#  class { ext4mount:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2013 Your name here, unless otherwise noted.
-#
-class ext4mount {
+# [*mountoptions*]
+#   mount options to be added to the fstab line.
 
+#
+define ext4mount (
+  $mountpoint,
+  $disk,
+  $mountoptions = 'errors=remount-ro'
+) {
 
+  mount { "mount-${mountpoint}":
+    ensure  => mounted,
+    name    => $mountpoint,
+    device  => $disk,
+    fstype  => 'ext4',
+    options => $mountoptions,
+    require => Exec["disk-${disk}-exists"],
+  }
+
+  # yes we know that this has no conditional "onlyif" or similar clause
+  # that's because the exec command is already inherently idempotent
+  # -- @philandstuff and @rjw1 20130604
+  exec { "disk-${disk}-exists":
+    command => "/sbin/e2label ${disk}"
+  }
 }
